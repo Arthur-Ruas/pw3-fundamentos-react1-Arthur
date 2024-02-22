94ee681320e71daee39a53c1468594146d683830
Arthur-Ruas <arthurruasafe@gmail.com>
2024-02-22 20:56:20 -0300

    Arthur: Card Css

--- fundamentos-react-1-arthur/src/components/CardBook/CardBook.js
+++ fundamentos-react-1-arthur/src/components/CardBook/CardBook.js
@@ -4,12 +4,16 @@ import './cardBook.css'
 const CardBook = ({title, author, image, description}) => {
   return (
     <div className="card-book">
-        <h3 className="card-book__title">{title}</h3>
-        <p className="card-book__author">{author}</p>
-
-        <img className="card-book__image" src={image} alt="Capa do livro as caverbas de aço de Isaac Asimov"/>
-
-        <p className="card-book__description">{description}</p>
+        <div className='card-book__esquerdo'>
+            <img className="card-book__image" src={image} alt="Capa do livro de Isaac Asimov"/>
+        </div>
+        <div className='card-book__direito'>
+            <header className='card-book__direito__header'>
+                <h3 className="card-book__title">{title}</h3>
+                <p className="card-book__author">por <span>{author}</span></p>
+            </header>
+            <p className="card-book__description">{description}</p>
+        </div>     
     </div>
   )
 }
